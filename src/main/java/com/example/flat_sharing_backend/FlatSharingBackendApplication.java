6a1c7595a4c38eb8db630e417ceafd84b9d31d6d
vKaverznev <kaverznev@gmail.com>
2023-06-15 11:37:40 +0200

    summary

---
+++ src/main/java/com/example/flat_sharing_backend/FlatSharingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flat_sharing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatSharingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatSharingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flat_sharing_backend/FlatSharingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flat_sharing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatSharingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
